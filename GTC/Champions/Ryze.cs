c4b5596f950d9c385748f546cd0435c2a3df6731
mar173 <kiker173@wp.pl>
2016-06-17 12:50:30 +0200

    OP RYZE REWORK

--- GTC/Champions/Ryze.cs
+++ GTC/Champions/Ryze.cs
@@ -2,17 +2,9 @@ using System;
 using System.Linq;
 using EloBuddy;
 using EloBuddy.SDK;
-using EloBuddy.SDK.Constants;
 using EloBuddy.SDK.Enumerations;
-using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
-using EloBuddy.SDK.Notifications;
-using EloBuddy.SDK.Rendering;
-using EloBuddy.SDK.Spells;
-using EloBuddy.SDK.ThirdParty;
-using EloBuddy.SDK.ThirdParty.Glide;
-using EloBuddy.SDK.Utils;
 
 namespace GTC.Champions
 {
@@ -20,7 +12,7 @@ namespace GTC.Champions
 	{
 		Menu menu;
 		
-		readonly Spell.Skillshot Q = new Spell.Skillshot(SpellSlot.Q, 900, SkillShotType.Linear, 250, 1400, 55) { AllowedCollisionCount = int.MaxValue, MinimumHitChance = HitChance.Low };
+		readonly Spell.Skillshot Q = new Spell.Skillshot(SpellSlot.Q, 900, SkillShotType.Linear, 250, 1400, 55) { AllowedCollisionCount = int.MaxValue, MinimumHitChance = HitChance.Medium };
 		
 		readonly Spell.Skillshot Q2 = new Spell.Skillshot(SpellSlot.Q, 900, SkillShotType.Linear, 250, 1400, 70) { AllowedCollisionCount = 0, MinimumHitChance = HitChance.High };
 		
@@ -30,7 +22,7 @@ namespace GTC.Champions
 		
 		readonly Spell.Active R = new Spell.Active(SpellSlot.R, 600);
 		
-		float lastaa, num, lastpasmove, cannext;
+		float num, cannext;
 		
 		bool canq, passive;
 		
@@ -39,11 +31,15 @@ namespace GTC.Champions
 		public Ryze()
 		{
 			menu = MainMenu.AddMenu("GTC Ryze", "gtcryze");
-			menu.Add("key", new KeyBind("Combo Key", false, KeyBind.BindTypes.HoldActive, ' '));
-			menu.Add("qmana", new Slider("Auto Q min. % Mana", 50, 5));
-			menu.Add("cann", new Slider("Next Spell[Q,W,E] Delay", 175, 0, 1000));
+			menu.AddGroupLabel("Addon made by Funboxxx");
+			menu.AddGroupLabel("BEST COMBO IS WHEN YOU HAVE 1 STACK AND QWER READY");
+			menu.AddSeparator();
+			menu.Add("next", new Slider("Delay Between Spells", 175, 150, 250));
+			menu.AddSeparator();
+			menu.Add("autoq", new CheckBox("Auto Q"));
+			menu.AddSeparator();
+			menu.Add("mana", new Slider("Auto Q min. % Mana", 75));
 			Game.OnTick += Game_OnTick;
-			Obj_AI_Base.OnBasicAttack += Obj_AI_Base_OnBasicAttack;
 			Obj_AI_Base.OnSpellCast += Obj_AI_Base_OnSpellCast;
 			Spellbook.OnCastSpell += Spellbook_OnCastSpell;
 			GameObject.OnCreate += GameObject_OnCreate;
@@ -52,14 +48,224 @@ namespace GTC.Champions
 		
 		void Game_OnTick(EventArgs args)
 		{
-			Combo();
+			try
+			{
+				if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+				{
+					if (!Q.IsReady() && !W.IsReady() && !E.IsReady() && !R.IsReady())
+					{
+						Orbwalker.DisableAttacking = false;
+						return;
+					}
+					Combo();
+				}
+				else
+				{
+					Orbwalker.DisableAttacking = false;
+					if (menu["autoq"].Cast<CheckBox>().CurrentValue && Orbwalker.CanMove)
+					{
+						AutoQ();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Chat.Print(e);
+			}
 		}
 		
-		void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+		void AutoQ()
 		{
-			if (sender.IsMe)
+			if (!Q.IsReady() || Player.Instance.Mana < Player.Instance.MaxMana * (menu["mana"].Cast<Slider>().CurrentValue / 100f))
 			{
-				lastaa = Game.Time * 1000;
+				return;
+			}
+			var t = TargetSelector.GetTarget(900, DamageType.Magical);
+			if (t == null)
+			{
+				return;
+			}
+			var pred2 = Q2.GetPrediction(t);
+			if (!pred2.CastPosition.IsZero)
+			{
+				Q.Cast(pred2.CastPosition);
+			}
+		}
+		
+		void CastQ()
+		{
+			if (!Q.IsReady() || Game.Time * 1000 < cannext + menu["next"].Cast<Slider>().CurrentValue)
+			{
+				return;
+			}
+			var t = TargetSelector.GetTarget(900, DamageType.Magical);
+			if (t == null)
+			{
+				return;
+			}
+			if ((!passive && stacks > 2) || passive)
+			{
+				var pred = Q.GetPrediction(t);
+				if (!pred.CastPosition.IsZero)
+				{
+					if (Orbwalker.CanMove)
+					{
+						Orbwalker.DisableAttacking = true;
+					}
+					Q.Cast(pred.CastPosition);
+				}
+			}
+			var pred2 = Q2.GetPrediction(t);
+			if (!pred2.CastPosition.IsZero)
+			{
+				if (Orbwalker.CanMove)
+				{
+					Orbwalker.DisableAttacking = true;
+				}
+				Q.Cast(pred2.CastPosition);
+			}
+		}
+		
+		void CastW()
+		{
+			if (!W.IsReady() || Game.Time * 1000 < cannext + menu["next"].Cast<Slider>().CurrentValue)
+			{
+				return;
+			}
+			if (Orbwalker.CanMove)
+			{
+				Orbwalker.DisableAttacking = true;
+			}
+			var t = TargetSelector.GetTarget(600, DamageType.Magical);
+			if (t == null)
+			{
+				return;
+			}
+			W.Cast(t);
+		}
+		
+		void CastE()
+		{
+			if (!E.IsReady() || Game.Time * 1000 < cannext + menu["next"].Cast<Slider>().CurrentValue)
+			{
+				return;
+			}
+			if (Orbwalker.CanMove)
+			{
+				Orbwalker.DisableAttacking = true;
+			}
+			var t = TargetSelector.GetTarget(600, DamageType.Magical);
+			if (t == null)
+			{
+				return;
+			}
+			E.Cast(t);
+		}
+		
+		void CastR()
+		{
+			if (!R.IsReady() || Game.Time * 1000 < cannext + menu["next"].Cast<Slider>().CurrentValue)
+			{
+				return;
+			}
+			if (Orbwalker.CanMove)
+			{
+				Orbwalker.DisableAttacking = true;
+			}
+			var t = TargetSelector.GetTarget(600, DamageType.Magical);
+			if (t == null)
+			{
+				return;
+			}
+			R.Cast();
+		}
+		
+		void Combo()
+		{
+			if (Game.Time * 1000 < cannext + menu["next"].Cast<Slider>().CurrentValue)
+			{
+				return;
+			}
+			if (passive)
+			{
+				if (canq)
+				{
+					CastQ();
+				}
+				else
+				{
+					if (R.IsReady())
+					{
+						CastR();
+					}
+					else if (W.IsReady())
+					{
+						CastW();
+					}
+					else if (E.IsReady())
+					{
+						CastE();
+					}
+				}
+			}
+			else
+			{
+				if (stacks == 0)
+				{
+					if (Q.IsReady() && W.IsReady() && E.IsReady() && R.IsReady())
+					{
+						CastQ();
+						return;
+					}
+					CastW();
+					CastE();
+					CastQ();
+				}
+				if (stacks == 1)
+				{
+					if (W.IsReady() && E.IsReady() && R.IsReady())
+					{
+						CastR();
+						return;
+					}
+					CastW();
+					CastE();
+					CastQ();
+					
+				}
+				if (stacks == 2)
+				{
+					if (R.IsReady() && (E.IsReady() || W.IsReady()))
+					{
+						CastR();
+						return;
+					}
+					if (W.IsReady() && E.IsReady())
+					{
+						CastE();
+						return;
+					}
+					CastW();
+					CastQ();
+				}
+				if (stacks == 3)
+				{
+					if (R.IsReady() && (Q.IsReady() || W.IsReady()))
+					{
+						CastR();
+						return;
+					}
+					CastE();
+					CastQ();
+					CastW();
+				}
+				if (stacks == 4)
+				{
+					CastR();
+					CastW();
+					CastQ();
+					CastE();
+				}
 			}
 		}
 		
@@ -146,293 +352,7 @@ namespace GTC.Champions
 			if (sender.Name == "Ryze_Base_P_Buf.troy")
 			{
 				passive = false;
-			}
-		}
-		
-		void Combo()
-		{
-			//try
-			AttackableUnit t = Target;
-			AIHeroClient qt = QTarget;
-			AIHeroClient wt = WERTarget;
-			int next = menu["cann"].Cast<Slider>().CurrentValue;
-			if (menu["key"].Cast<KeyBind>().CurrentValue)
-			{
-				Orbwalker.DisableAttacking = true;
-				Orbwalker.DisableMovement = true;
-				if (passive && Player.Instance.Mana > 200)
-				{
-					if (Game.Time * 1000 > lastpasmove + 250)
-					{
-						Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
-						lastpasmove = Game.Time * 1000;
-					}
-
-					if (canq)
-					{
-						if (qt != null)
-						{
-							if (Game.Time * 1000 > cannext + next)
-							{
-								Q.Cast(qt);
-							}
-						}
-					}
-					else if (wt != null)
-					{
-						if (R.IsReady())
-						{
-							R.Cast();
-						}
-						else if (W.IsReady())
-						{
-							if (Game.Time * 1000 > cannext + next)
-							{
-								W.Cast(wt);
-							}
-						}
-						else if (E.IsReady())
-						{
-							if (Game.Time * 1000 > cannext + next)
-							{
-								E.Cast(wt);
-							}
-						}
-					}
-				}
-				else
-				{
-					if (Game.Time * 1000 > lastaa + (Player.Instance.AttackDelay * 1000) - (Game.Ping * 2.15f))
-					{
-						if (t != null && ((stacks < 3 && !W.IsReady() && !E.IsReady()) || (stacks > 2 && !Q.IsReady() && !W.IsReady() && !E.IsReady())))
-						{
-							Player.IssueOrder(GameObjectOrder.AttackUnit, t);
-						}
-						else if (Game.Time * 1000 > lastaa + (Player.Instance.AttackCastDelay * 1000) - (Game.Ping / 2.15f) + (Player.Instance.AttackSpeedMod * 10))
-						{
-							Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
-							if (Q.IsReady() && !W.IsReady())
-							{
-								if (qt != null)
-								{
-									if (Game.Time * 1000 > cannext + next)
-									{
-										Q2.Cast(qt);
-									}
-								}
-							}
-						}
-					}
-					else if (Game.Time * 1000 > lastaa + (Player.Instance.AttackCastDelay * 1000) - (Game.Ping / 2.15f) + (Player.Instance.AttackSpeedMod * 10))
-					{
-						Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
-					}
-					CastSpells(qt, wt, next);
-				}
-			}
-			else
-			{
-				Orbwalker.DisableAttacking = false;
-				Orbwalker.DisableMovement = false;
-				if (Q.IsReady() && qt != null && Player.Instance.Mana > Player.Instance.MaxMana * (menu["qmana"].Cast<Slider>().CurrentValue / 100f))
-				{
-					Q2.Cast(qt);
-				}
-			}
-			//}
-			//catch(Exception e)
-			//{
-			//Chat.Print(e);
-			//}
-		}
-		
-		void CastSpells(AIHeroClient qt, AIHeroClient wt, float next)
-		{
-			if (menu["key"].Cast<KeyBind>().CurrentValue && !passive)
-			{
-				bool qready = Q.IsReady();
-				bool wready = W.IsReady();
-				bool eready = E.IsReady();
-				bool rready = R.IsReady();
-				if (stacks < 3)
-				{
-					if (wt != null)
-					{
-						if (wready && Game.Time * 1000 > cannext + next)
-						{
-							W.Cast(wt);
-							wready = false;
-						}
-						else if (eready && Game.Time * 1000 > cannext + next)
-						{
-							E.Cast(wt);
-							eready = false;
-						}
-						else if (qready && Game.Time * 1000 > cannext + next)
-						{
-							Q2.Cast(wt);
-							qready = false;
-						}
-					}
-					else if (qready && !wready && qt != null)
-					{
-						Q2.Cast(qt);
-						qready = false;
-					}
-				}
-				if (stacks == 3)
-				{
-					if (wt != null)
-					{
-						if (R.IsReady())
-						{
-							if (wready && Game.Time * 1000 > cannext + next)
-							{
-								W.Cast(wt);
-								wready = false;
-							}
-							else if (eready && Game.Time * 1000 > cannext + next)
-							{
-								E.Cast(wt);
-								eready = false;
-							}
-							else if (qready && Game.Time * 1000 > cannext + next)
-							{
-								Q.Cast(wt);
-								qready = false;
-							}
-						}
-						else if (wready)
-						{
-							if (rready)
-							{
-								R.Cast();
-								rready = false;
-							}
-							else if (eready && Game.Time * 1000 > cannext + next)
-							{
-								E.Cast(wt);
-								eready = false;
-							}
-							else if (qready && Game.Time * 1000 > cannext + next)
-							{
-								Q.Cast(wt);
-								qready = false;
-							}
-							else if (Game.Time * 1000 > cannext + next)
-							{
-								W.Cast(wt);
-								wready = false;
-							}
-						}
-						else
-						{
-							if (eready && Game.Time * 1000 > cannext + next)
-							{
-								E.Cast(wt);
-								eready = false;
-							}
-							else if (qready && Game.Time * 1000 > cannext + next)
-							{
-								Q.Cast(wt);
-								qready = false;
-							}
-						}
-					}
-					else if (qready && !wready && qt != null)
-					{
-						Q2.Cast(qt);
-						qready = false;
-					}
-				}
-				if (stacks == 4)
-				{
-					if (wt != null)
-					{
-						if (rready)
-						{
-							R.Cast();
-							rready = false;
-						}
-						else if (wready && Game.Time * 1000 > cannext + next)
-						{
-							W.Cast(wt);
-							wready = false;
-						}
-						else if (eready && Game.Time * 1000 > cannext + next)
-						{
-							E.Cast(wt);
-							eready = false;
-						}
-						else if (qready && Game.Time * 1000 > cannext + next)
-						{
-							Q.Cast(wt);
-							qready = false;
-						}
-					}
-					else if (qready && !wready && qt != null)
-					{
-						Q2.Cast(qt);
-						qready = false;
-					}
-				}
-			}
-		}
-		
-		AttackableUnit Target
-		{
-			get
-			{
-				int health = int.MaxValue;
-				AttackableUnit t = null;
-				foreach (var target in EntityManager.Heroes.Enemies.Where(x => x.IsValidTarget(Player.Instance.AttackRange + Player.Instance.BoundingRadius + x.BoundingRadius)))
-				{
-					int hp = (int)(target.Health * (100 / (100 + target.Armor)));
-					if (hp < health)
-					{
-						health = hp;
-						t = target;
-					}
-				}
-				return t;
-			}
-		}
-		
-		AIHeroClient QTarget
-		{
-			get
-			{
-				int health = int.MaxValue;
-				AIHeroClient t = null;
-				foreach (var target in EntityManager.Heroes.Enemies.Where(x => x.IsValidTarget(900)))
-				{
-					int hp = (int)(target.Health * (100 / (100 + target.MagicShield)));
-					if (hp < health)
-					{
-						health = hp;
-						t = target;
-					}
-				}
-				return t;
-			}
-		}
-		
-		AIHeroClient WERTarget
-		{
-			get
-			{
-				int health = int.MaxValue;
-				AIHeroClient t = null;
-				foreach (var target in EntityManager.Heroes.Enemies.Where(x => x.IsValidTarget(600)))
-				{
-					int hp = (int)(target.Health * (100 / (100 + target.MagicShield)));
-					if (hp < health)
-					{
-						health = hp;
-						t = target;
-					}
-				}
-				return t;
+				stacks = 0;
 			}
 		}
 	}
